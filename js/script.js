ef80e95de908418e9d1597371fd1db47111d23ec
yathomasi1 <yathomasi1@gmail.com>
2018-05-27 12:51:18 +0545

    Initial commit for simplertc

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB-HI3oUCu5hZUnPTiqviyxp2vr7uM_Kj4",
+    authDomain: "vidliv-1.firebaseapp.com",
+    databaseURL: "https://vidliv-1.firebaseio.com",
+    projectId: "vidliv-1",
+    storageBucket: "vidliv-1.appspot.com",
+    messagingSenderId: "446347692704"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database().ref();
+var yourVideo = document.getElementById('yourVideo');
+var friendsVideo = document.getElementById("friendsVideo");
+var yourId = Math.floor(Math.random()*1000000000);
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': '49m3WMYqjVKS','username': 'yathomasi1@gmail.com'}]};
+var pc = new RTCPeerConnection(servers);
+pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice':event.candidate})):console.log("Sent all Ice") );
+pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+
+function sendMessage(senderId, data) {
+ var msg = database.push({ sender: senderId, message: data });
+ msg.remove();
+}
+
+function readMessage(data) {
+ var msg = JSON.parse(data.val().message);
+ var sender = data.val().sender;
+ if (sender != yourId) {
+ if (msg.ice != undefined)
+ pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+ else if (msg.sdp.type == "offer")
+ pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+ .then(() => pc.createAnswer())
+ .then(answer => pc.setLocalDescription(answer))
+ .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+ else if (msg.sdp.type == "answer")
+ pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+ }
+};
+
+database.on('child_added', readMessage);
+
+function showMyFace() {
+ navigator.mediaDevices.getUserMedia({audio:true, video:true})
+ .then(stream => yourVideo.srcObject = stream)
+ .then(stream => pc.addStream(stream));
+}
+
+function showFriendsFace() {
+ pc.createOffer()
+ .then(offer => pc.setLocalDescription(offer) )
+ .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+}
